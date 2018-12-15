c5b8edebf4cb0fac819627f2abcaab55a4a4082a
Aaron Scherer <aequasi@gmail.com>
2018-12-15 13:14:14 -0800

    fix(deploy): Fixing deploy

--- src/Entity/Permission.ts
+++ src/Entity/Permission.ts
@@ -1,28 +1,30 @@
 import {BaseEntity, Column, Entity, Index, PrimaryGeneratedColumn} from 'typeorm';
 
-export enum PermissionType {
+enum PermissionType {
     User = 1,
     Role = 2,
 }
 
-@Entity("Permission")
+@Entity('Permission')
 export class Permission extends BaseEntity {
+    public static PermissionType = PermissionType;
+
     @PrimaryGeneratedColumn()
     public Id: number;
 
-    @Column({type: "bigint"}) @Index("guild", ["GuildID"])
+    @Column({type: 'bigint'}) @Index('guild', ['GuildID'])
     public GuildId: string;
 
-    @Column({length: 512}) @Index("node", ["Node"])
+    @Column({length: 512}) @Index('node', ['Node'])
     public Node: string;
 
-    @Column() @Index("type", ["Type"])
+    @Column() @Index('type', ['Type'])
     public Type: PermissionType;
 
-    @Column({type: "bigint"}) @Index("type_id", ["TypeId"])
+    @Column({type: 'bigint'}) @Index('type_id', ['TypeId'])
     public TypeId: string;
 
-    @Column() @Index("allowed", ["Allowed"])
+    @Column() @Index('allowed', ['Allowed'])
     public Allowed: boolean = true;
 
     public constructor(init?: Partial<Permission>) {
